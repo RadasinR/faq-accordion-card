b6126b1ec494f7fa073a9f3dcd86471563a8accf
Igor <87859179+RadasinR@users.noreply.github.com>
2023-04-30 19:27:49 +0200

    fixed the  card

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 function rotateArrow(sum) {
-    let arrow = sum.querySelector('.details-marker');
-    arrow.classList.toggle('rotate180');
-    sum.classList.toggle('text--dark');
-    sum.classList.toggle('fw-700');
+  let summarys = document.querySelectorAll("summary");
+
+  for (let s of summarys) {
+    if (s === sum) {
+      toggleActiveState(s);
+      continue;
+    }
+
+    if (s.classList.contains("fw-700")) {
+      toggleActiveState(s);
+      s.parentElement.open = false;
+    }
+  }
+}
+
+function toggleActiveState(summary) {
+  let arrow = summary.querySelector(".details-marker");
+  arrow.classList.toggle("rotate180");
+  summary.classList.toggle("text--dark");
+  summary.classList.toggle("fw-700");
 }
